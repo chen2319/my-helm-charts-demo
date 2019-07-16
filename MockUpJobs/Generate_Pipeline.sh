814c25df0380b0ce0b3e6c125f06d6189e53d9c5
Yang Chen (yangch3) <yangch3@cisco.com>
2019-07-16 17:34:21 +0800

    add shell mock job

---
+++ MockUpJobs/Generate_Pipeline.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+WORK_HOME=/Users/yangchen/IntelliJIDEAProjects/my-helm-charts-demo
+VERSION=1.0.0
+PIPELINE_VALUE_URL=https://raw.githubusercontent.com/chen2319/my-helm-charts-demo/master/deploy_values/yangdemo_pipeline_values_$VERSION.json
+
+curl -s $PIPELINE_VALUE_URL -o $WORK_HOME/MockUpJobs/log/yangdemo_pipeline_values_$VERSION.json
+
+spin pt plan --file $WORK_HOME/MockUpJobs/log/yangdemo_pipeline_values_$VERSION.json | tee $WORK_HOME/MockUpJobs/log/deploy_pipeline_$VERSION.json
+
+spin pipeline save --file $WORK_HOME/MockUpJobs/log/deploy_pipeline_$VERSION.json
+
